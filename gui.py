bba0c5967939f9cc42c0ff66f60f41c01b5f3f85
mezez <ekemammezez@gmail.com>
2021-11-06 14:05:29 +0100

    initial commit

---
+++ gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+# create a label widget
+titleLabel = Label(root, text="Advanced algorithm Project")
+# add the widget to the screen
+titleLabel.pack()
+
+root.mainloop()
